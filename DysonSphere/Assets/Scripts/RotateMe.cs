45f559c991a7abdbb2397a82ba1c98b254f53cba
Brent Bartlett <brent.bartlett@gmail.com>
2019-01-26 09:39:27 -0800

    Update RotateMe.cs
     Made ship thrustable.

--- DysonSphere/Assets/Scripts/RotateMe.cs
+++ DysonSphere/Assets/Scripts/RotateMe.cs
@@ -6,25 +6,22 @@ public class RotateMe : MonoBehaviour
 {
     private float _maxRotSpeed = 200f;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
     // Update is called once per frame
     void Update()
     {
         float translation = Input.GetAxis("Horizontal") * _maxRotSpeed;
-        Vector3 point = new Vector3(0, 0, 0);
-
-
+        
         Vector3 axis;
         if(translation > 0)
             axis = new Vector3(0, 0, -1);
         else
             axis = new Vector3(0, 0, 1);
 
-        transform.RotateAround(point, axis, Time.deltaTime * Mathf.Abs(translation));
+        transform.RotateAround(transform.parent.position, axis, Time.deltaTime * Mathf.Abs(translation));
+
+        float thrust = Input.GetAxis("Vertical");
+        Vector2 direction = transform.localPosition.normalized;
+        var prb = transform.parent.GetComponent<Rigidbody2D>();
+        prb.AddForce(direction * -10f * thrust);
     }
 }
