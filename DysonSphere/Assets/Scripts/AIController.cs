88d1c5343a68b049f45de95bf20c5b56d8d12956
albueche <albert.bueche.iv@gmail.com>
2019-01-26 10:07:14 -0800

    Added basic planet and AI find nearest planet logic

--- DysonSphere/Assets/Scripts/AIController.cs
+++ DysonSphere/Assets/Scripts/AIController.cs
@@ -5,4 +5,36 @@ using UnityEngine;
 public class AIController : MonoBehaviour
 {
 
+    private void Update()
+    {
+        Debug.Log(GetClosestPlanet(transform.position));
+    }
+
+    private Vector3 GetClosestPlanet(Vector3 source)
+    {
+        return GetClosest(source, Planet.PlanetPositions);
+    }
+
+    private Vector3 GetClosest(Vector3 source, Vector3[] targets)
+    {
+        Vector3 closest = Vector3.zero;
+        float closestSquareDistance = float.MaxValue;
+        foreach (Vector3 target in targets)
+        {
+            Vector3 toTarget = target - source;
+            float sqrToTargetDistance = toTarget.sqrMagnitude;
+
+            if (sqrToTargetDistance < closestSquareDistance)
+            {
+                closest = target;
+                closestSquareDistance = sqrToTargetDistance;
+            }
+        }
+        return closest;
+    }
+
+    private void Go(Vector3 target)
+    {
+
+    }
 }
---
+++ DysonSphere/Assets/Scripts/Planet.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class Planet : MonoBehaviour
+{
+    private static List<Planet> planets = new List<Planet>();
+    public static Vector3[] PlanetPositions { get { return planets.Select(x => x.transform.position).ToArray(); } }
+
+    void Start()
+    {
+        planets.Add(this);
+    }
+
+    void Update()
+    {
+
+    }
+}
