6fcc10c3629eb63bf15cecf4859bfa89f026d91b
Brent Bartlett <brent.bartlett@gmail.com>
2019-01-26 09:10:37 -0800

    Ship Prefab
    Added a test scene.
    
    Brought in ship prefab, scripts for rotation, and sprite.

---
+++ DysonSphere/Assets/Scripts/RotateMe.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateMe : MonoBehaviour
+{
+    private float _maxRotSpeed = 200f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float translation = Input.GetAxis("Horizontal") * _maxRotSpeed;
+        Vector3 point = new Vector3(0, 0, 0);
+
+
+        Vector3 axis;
+        if(translation > 0)
+            axis = new Vector3(0, 0, -1);
+        else
+            axis = new Vector3(0, 0, 1);
+
+        transform.RotateAround(point, axis, Time.deltaTime * Mathf.Abs(translation));
+    }
+}
