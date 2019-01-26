f47e7d80756277ed5719ee5fed6b466b71e0bd51
albueche <albert.bueche.iv@gmail.com>
2019-01-26 14:40:40 -0800

    Scene controller populates AI ships and planets

--- DysonSphere/Assets/Scripts/GameController.cs
+++ DysonSphere/Assets/Scripts/GameController.cs
@@ -4,15 +4,27 @@ using UnityEngine;
 
 public class GameController : MonoBehaviour
 {
-    // Start is called before the first frame update
+
+    public GameObject planetPrefab;
+    public GameObject aiShipPrefab;
+
+    public Transform planetWrapper;
+    public Transform aiShipWrapper;
+
     void Start()
     {
-        
-    }
+        for (int i = 0; i < 11; i++)
+        {
+            float x = 10 * (i - 5) + Random.Range(1, 9);
+            float y = Random.Range(-50, 50);
+            Instantiate(planetPrefab, new Vector3(x, y, 0), Quaternion.identity, planetWrapper);
+        }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        for (int i = 0; i < 11; i++)
+        {
+            float x = 10 * (i - 5) + Random.Range(1, 9);
+            float y = Random.Range(-50, 50);
+            Instantiate(aiShipPrefab, new Vector3(x, y, 0), Quaternion.identity, aiShipWrapper);
+        }
     }
 }
