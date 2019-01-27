54ee24d8aea3a57243eac9a7755652d68e7846b9
jovlett <44935570+jovlett@users.noreply.github.com>
2019-01-26 19:59:28 -0800

    Added shooting
    rootin tootin cowboy shootin

---
+++ DysonSphere/Assets/Bullet.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    private Rigidbody2D rb2d;
+    public int speed;
+
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+        Destroy(gameObject, 6);
+    }
+
+    void Update()
+    {
+        rb2d.velocity = transform.up * speed;
+    }
+}
