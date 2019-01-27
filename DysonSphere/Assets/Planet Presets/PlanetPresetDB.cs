dcf1d82369d97c8e8a009f2b26870a79838a48ff
jovlett <44935570+jovlett@users.noreply.github.com>
2019-01-27 08:43:41 -0800

    Removed duplicate scripts.
    Removed duplicate scripts.

--- DysonSphere/Assets/Planet Presets/PlanetPresetDB.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "Planet Preset", menuName = "Planet Presets/Planet Preset DB", order = 2)]
-public class PlanetPresetDB : ScriptableObject
-{
-    public PlanetPreset[] planets;
-}
