965863268a603f2c1e2a74302a81fea79be59c83
jovlett <44935570+jovlett@users.noreply.github.com>
2019-01-26 21:40:27 -0800

    planet preset system
    gumball rn

--- DysonSphere/Assets/Scripts/Ship.cs
+++ DysonSphere/Assets/Scripts/Ship.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class Ship : MonoBehaviour
 {
+    public PlanetPresetDB planetDB;
+    public SpriteRenderer[] landDetailSpr;
+    public SpriteRenderer[] waterSpr;
+    public SpriteRenderer[] windSpr;
     private List<Resource> resources = new List<Resource>();
 
     public float TotalResources
@@ -21,14 +25,21 @@ public class Ship : MonoBehaviour
 
     private void Start()
     {
+        setPreset();
+
         GameState.Instance.AddShip(this);
         resources.Add(new Resource(Resource.Type.Stuffium, 100));
     }
 
+    public void AddRandomResource(float amount)
+    {
+        int resourceIndex = Random.Range(0, resources.Count);
+        resources[resourceIndex].amount += amount;
+    }
+
     public Resource TakeResource(float amount)
     {
-        int count = resources.Count;
-        int resourceIndex = Random.Range(0, count);
+        int resourceIndex = Random.Range(0, resources.Count);
         if (resources[resourceIndex].amount < amount)
         {
             amount = 0;
@@ -37,5 +48,32 @@ public class Ship : MonoBehaviour
         resources[resourceIndex].amount -= amount;
 
         return new Resource(resources[resourceIndex].type, amount);
+    }
+
+    private void setPreset()
+    {
+        int i = Random.Range(0, planetDB.planets.Length);
+        var landSpr = GetComponent<SpriteRenderer>();
+
+        landSpr.color = planetDB.planets[i].landColor;
+
+        windSpr[1].transform.parent.GetComponent<ScrollMaterial>().speed = planetDB.planets[i].windspeed;
+        landDetailSpr[1].transform.parent.GetComponent<ScrollMaterial>().speed = planetDB.planets[i].spinspeed;
+
+        foreach (SpriteRenderer sp in landDetailSpr)
+        {
+            sp.color = planetDB.planets[i].landDetailColor;
+        }
+
+        foreach (SpriteRenderer sp in waterSpr)
+        {
+            sp.color = planetDB.planets[i].waterColor;
+        }
+
+        foreach (SpriteRenderer sp in windSpr)
+        {
+            sp.color = planetDB.planets[i].cloudColor;
+        }
+
     }
 }
