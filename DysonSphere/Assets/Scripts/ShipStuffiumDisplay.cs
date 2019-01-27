b5df57c7778ee4f406519b3556f4c619777d84fd
Brent Bartlett <brent.bartlett@gmail.com>
2019-01-27 12:15:00 -0800

    Added stuffium display to ships.
    Stuffium display to ships.

---
+++ DysonSphere/Assets/Scripts/ShipStuffiumDisplay.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ShipStuffiumDisplay : MonoBehaviour
+{
+    Text _resourceDisplay;
+    Ship _myShip;
+
+    void Awake()
+    {
+        _resourceDisplay = GetComponent<Text>();
+        _myShip = GetComponentInParent<Ship>();
+    }
+
+    void Update()
+    {
+        _resourceDisplay.text = "x<b>" + Mathf.FloorToInt(_myShip.TotalResources) + "</b>";
+    }
+}
