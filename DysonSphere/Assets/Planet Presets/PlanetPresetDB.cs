bbabc47b96a8a190af48c1477c6f48a4a3a7055a
Brent Bartlett <brent.bartlett@gmail.com>
2019-01-27 08:51:00 -0800

    Revert "Removed duplicate scripts."
    This reverts commit dcf1d82369d97c8e8a009f2b26870a79838a48ff.

---
+++ DysonSphere/Assets/Planet Presets/PlanetPresetDB.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Planet Preset", menuName = "Planet Presets/Planet Preset DB", order = 2)]
+public class PlanetPresetDB : ScriptableObject
+{
+    public PlanetPreset[] planets;
+}
