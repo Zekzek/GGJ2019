cc2a014bb540b34de8aca73a6b098ee690ccd26c
Brent Bartlett <brent.bartlett@gmail.com>
2019-01-27 12:17:56 -0800

    Added stuffium display to planets.

---
+++ DysonSphere/Assets/Scripts/PlanetStuffiumDisplay.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlanetStuffiumDisplay : MonoBehaviour
+{
+    Text _resourceDisplay;
+    Planet _myPlanet;
+
+    void Awake()
+    {
+        _resourceDisplay = GetComponent<Text>();
+        _myPlanet = GetComponentInParent<Planet>();
+    }
+
+    void Update()
+    {
+        _resourceDisplay.text = "x<b>" + Mathf.FloorToInt(_myPlanet.TotalResources) + "</b>";
+    }
+}
